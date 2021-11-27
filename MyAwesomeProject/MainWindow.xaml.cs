514dad111644965770aa9401ce0442fec0919350
Емельянов Сергей <esn@medinfocenter.ru>
2021-11-27 08:31:14 +0500

    first commit

---
+++ MyAwesomeProject/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+
+namespace MyAwesomeProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+#if DEBUG
+            Debug.WriteLine("Hello from debug");
+#endif
+        }
+    }
+}
